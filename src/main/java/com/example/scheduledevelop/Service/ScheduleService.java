6437d1a94450f8a4c587a1a475d4d090a9d0c0e0
HS <ehehqp@gmail.com>
2026-04-16 16:10:30 +0900

    제목 : 일정 조회 구현 완료 본문 :

--- src/main/java/com/example/scheduledevelop/Service/ScheduleService.java
+++ src/main/java/com/example/scheduledevelop/Service/ScheduleService.java
@@ -2,11 +2,13 @@ package com.example.scheduledevelop.Service;
 
 import com.example.scheduledevelop.DTO.CreatscheduleRequest;
 import com.example.scheduledevelop.DTO.CreatscheduleResponse;
+import com.example.scheduledevelop.DTO.GetscheduleResponse;
 import com.example.scheduledevelop.Entity.ScheduleEntity;
 import com.example.scheduledevelop.Repsitory.ScheduleRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Getter
 @Service
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Service;
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
 
+    // 일정 생성
+    @Transactional
     public CreatscheduleResponse save(CreatscheduleRequest request) {
 
         ScheduleEntity schedule = new ScheduleEntity(request.getTitle(),request.getContent(),request.getAuthorName());
@@ -27,5 +31,22 @@ public class ScheduleService {
                 schedule.getAuthorName(),
                 schedule.getCreatedAt(),
                 schedule.getModifiedAt());
+    }
+
+    // 일정 단건 조회
+    @Transactional
+    public GetscheduleResponse getOne(Long id) {
+        ScheduleEntity schedule = scheduleRepository.findById(id).orElseThrow(
+                () -> new IllegalStateException("일정을 찾을 수 없습니다.")
+        );
+
+        return new GetscheduleResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getAuthorName(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt()
+        );
     }
 }
