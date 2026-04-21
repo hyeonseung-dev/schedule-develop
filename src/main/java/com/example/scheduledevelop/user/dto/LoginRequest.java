7735688e5ab27d0f158a2984a96f00cc208baeef
HS <ehehqp@gmail.com>
2026-04-21 14:33:10 +0900

    제목 : 이메일 형식 정규식 구현 본문 :

--- src/main/java/com/example/scheduledevelop/user/dto/LoginRequest.java
+++ src/main/java/com/example/scheduledevelop/user/dto/LoginRequest.java
@@ -1,11 +1,13 @@
 package com.example.scheduledevelop.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 
 public class LoginRequest {
+    @Email(message = "올바르지 않은 이메일 형식입니다.")
     private String email;
     @Size(min = 9, message = "비밀번호는 {min}자 이상이어야 합니다")
     private String password;
