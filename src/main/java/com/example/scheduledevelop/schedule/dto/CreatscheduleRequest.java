40f209c2e9f3fc366b83ef0cdc29bc72ab5f012d
HS <ehehqp@gmail.com>
2026-04-21 14:36:00 +0900

    제목 : 일정 제목 예외처리 본문 :

--- src/main/java/com/example/scheduledevelop/schedule/dto/CreatscheduleRequest.java
+++ src/main/java/com/example/scheduledevelop/schedule/dto/CreatscheduleRequest.java
@@ -1,10 +1,12 @@
 package com.example.scheduledevelop.schedule.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 
 public class CreatscheduleRequest {
+    @Size(max = 10, message = "제목은 10글자 이내여야합니다.")
     private String title;
     private String content;
 }
--- src/main/java/com/example/scheduledevelop/user/dto/LoginRequest.java
+++ src/main/java/com/example/scheduledevelop/user/dto/LoginRequest.java
@@ -1,13 +1,17 @@
 package com.example.scheduledevelop.user.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 
 public class LoginRequest {
-    @Email(message = "올바르지 않은 이메일 형식입니다.")
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
+            message = "올바른 이메일 형식이 아닙니다."
+    )
     private String email;
     @Size(min = 9, message = "비밀번호는 {min}자 이상이어야 합니다")
     private String password;
